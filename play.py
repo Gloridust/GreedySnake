54ecb2a7d8881b8ba7fe9cf741bf5a471d232be6
Gloridust <Gloridust@gmail.com>
2023-12-08 00:40:06 +0800

    Update play.py

--- play.py
+++ play.py
@@ -17,15 +17,11 @@ def main():
 
     # 初始化屏幕
     screen = pygame.display.set_mode((WIDTH, HEIGHT))
-    pygame.display.set_caption("贪吃蛇游戏")
+    pygame.display.set_caption("GreedySnake-beginners")
 
     # 初始化时钟
     clock = pygame.time.Clock()
 
-    while True:
-        run_game(screen, clock)
-
-def run_game(screen, clock):
     # 初始化贪吃蛇
     snake = [(100, 100), (90, 100), (80, 100)]
     snake_dir = (GRID_SIZE, 0)
@@ -36,17 +32,9 @@ def run_game(screen, clock):
     # 计分板
     score = 0
 
-    # 游戏进行标志
-    game_over_flag = False
-
-    while not game_over_flag:
-        snake_dir, game_over_flag = handle_events(snake_dir, game_over_flag)
-
-        # 如果游戏结束，等待按键
-        if game_over_flag:
-            render_game_over(screen, score)
-            wait_for_key()
-            return  # 重新开始游戏
+    # 游戏主循环
+    while True:
+        snake_dir = handle_events(snake_dir)
 
         # 移动贪吃蛇
         head = move_snake(snake, snake_dir)
@@ -60,7 +48,7 @@ def run_game(screen, clock):
 
         # 判断是否碰到边界或自己
         if is_collision(head, snake):
-            game_over_flag = True
+            game_over(score)
 
         # 渲染屏幕
         render_screen(screen, snake, food, score)
@@ -68,7 +56,7 @@ def run_game(screen, clock):
         # 控制帧率
         clock.tick(FPS)
 
-def handle_events(snake_dir, game_over_flag):
+def handle_events(snake_dir):
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             pygame.quit()
@@ -76,17 +64,15 @@ def handle_events(snake_dir, game_over_flag):
 
     keys = pygame.key.get_pressed()
     if keys[pygame.K_UP] and snake_dir != (0, GRID_SIZE):
-        return (0, -GRID_SIZE), game_over_flag
+        return (0, -GRID_SIZE)
     elif keys[pygame.K_DOWN] and snake_dir != (0, -GRID_SIZE):
-        return (0, GRID_SIZE), game_over_flag
+        return (0, GRID_SIZE)
     elif keys[pygame.K_LEFT] and snake_dir != (GRID_SIZE, 0):
-        return (-GRID_SIZE, 0), game_over_flag
+        return (-GRID_SIZE, 0)
     elif keys[pygame.K_RIGHT] and snake_dir != (-GRID_SIZE, 0):
-        return (GRID_SIZE, 0), game_over_flag
-    elif keys[pygame.K_SPACE] and game_over_flag:
-        return (GRID_SIZE, 0), False  # 重新开始游戏
+        return (GRID_SIZE, 0)
     else:
-        return snake_dir, game_over_flag
+        return snake_dir
 
 def move_snake(snake, snake_dir):
     head = (snake[0][0] + snake_dir[0], snake[0][1] + snake_dir[1])
@@ -119,32 +105,10 @@ def render_screen(screen, snake, food, score):
 
     pygame.display.flip()
 
-def render_game_over(screen, score):
-    screen.fill(WHITE)
-
-    font = pygame.font.Font(None, 72)
-    game_over_text = font.render("Game Over", True, (255, 0, 0))
-    score_text = font.render(f"Score: {score}", True, (0, 0, 0))
-
-    screen.blit(game_over_text, (WIDTH // 2 - game_over_text.get_width() // 2, HEIGHT // 2 - 50))
-    screen.blit(score_text, (WIDTH // 2 - score_text.get_width() // 2, HEIGHT // 2 + 50))
-
-    pygame.display.flip()
-
-def wait_for_key():
-    waiting = True
-    while waiting:
-        for event in pygame.event.get():
-            if event.type == pygame.QUIT:
-                pygame.quit()
-                sys.exit()
-            elif event.type == pygame.KEYDOWN:
-                waiting = False
-
-        pygame.time.delay(50)  # 添加延迟以减轻 CPU 负载
-
-    pygame.event.clear()  # 清除事件队列，防止按键被重复处理
-
+def game_over(score):
+    print(f"Game Over! Your Score: {score}")
+    pygame.quit()
+    sys.exit()
 
 if __name__ == "__main__":
     main()
